c0f6f5981d6ce83912174a092a783e6cbd1b21f0
Eric Lopes <olv.ericlopes@gmail.com>
2021-11-26 20:51:04 -0300

    Data output and fixation exercise!

---
+++ DataOutput/DataOutput/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace DataOutput {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // write
+            // no break line
+            Console.Write("Bom dia!");
+
+            // writeline
+            // breakline
+            Console.WriteLine("Boa noite!");
+            Console.WriteLine("Boa tarde!");
+
+            // printing variables
+            char genre = 'f';
+            int age = 32;
+            double balance = 10.536;
+            string name = "maria";
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine(genre);
+            Console.WriteLine(age);
+            Console.WriteLine(balance);
+            Console.WriteLine(name);
+            // controlling the decimal places
+            Console.WriteLine(balance.ToString("F2"));// two decimal places
+            Console.WriteLine(balance.ToString("F4"));// four decimal places
+            // printing with .
+            Console.WriteLine(balance.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("----------------------");
+
+            //----------------------------------------------------//
+
+            // placeholders
+            Console.WriteLine("{0} have " +
+                "{1} years old and have balance equals a " +
+                "{2:F2} dollars", name, age, balance);
+
+            // interpolation
+            Console.WriteLine($"{name} have " +
+                $"{age} years old and have balance equals a " +
+                $"{balance:F2} dollars");
+
+            // concatenation
+            Console.WriteLine
+                (name +
+                " have " +
+                age +
+                " years old and have balance equals a " + 
+                balance.ToString("F2", CultureInfo.InvariantCulture) +
+                " dollars");
+
+        }
+    }
+}
---
+++ FixExerciseDataOutput/FixExerciseDataOutput/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixExerciseDataOutput {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // fixation exercise
+
+            string product1 = "Computer";
+            string product2 = "Office desk";
+
+            byte age = 30;
+            int id = 5290;
+            char genre = 'M';
+
+            double price1 = 2100.00;
+            double price2 = 650.50;
+            double measure = 53.234567;
+
+            Console.WriteLine("Products:");
+            Console.WriteLine(product1 + ", whose price is $ " + price1);
+            Console.WriteLine(product2 + ", whose price is $ " + price2);
+            Console.WriteLine();
+            Console.WriteLine("Record: " + age + " years old, id " + id + " and genre: " + genre);
+            Console.WriteLine();
+            Console.WriteLine("Measure whit eight decimal places: " + measure.ToString("F8"));
+            Console.WriteLine("Rounded (three decimal places): " + measure.ToString("F3"));
+            Console.WriteLine("Decimal separator invariant culture: " + measure.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
