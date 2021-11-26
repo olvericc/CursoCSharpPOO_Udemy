1d1c926195942d0846f02e759ec720f57a3e73ee
Eric Lopes <olv.ericlopes@gmail.com>
2021-11-26 12:31:08 -0300

    First Project in C# date: 26/11/2021

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello world, my first code in c#");
+            int x = 10;
+            int y = 20;
+            int sum = x + y;
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
+
+
