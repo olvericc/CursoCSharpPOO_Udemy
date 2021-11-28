9540fc98eedfee60de6d6ac2c7852991acac568e
Eric Lopes <olv.ericlopes@gmail.com>
2021-11-28 20:41:20 -0300

    Data input part 2 day: 28/11/2021

---
+++ DataInput2/DataInput2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace DataInput2 {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            // Reading a int number 
+            
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("you write: " + n1);
+                    
+            // Reading a char
+            
+            char ch = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("you write: " + ch);
+
+            // Reading a double number using CultureInfo
+
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("you write: " + n2.ToString("F2" ,CultureInfo.InvariantCulture));
+
+            // example reading a string, char, int and double 
+            // split
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string name = vet[0];
+            char sex = char.Parse(vet[1]);
+            int age = int.Parse(vet[2]);
+            double height = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("-----------");
+            Console.WriteLine("you write: ");
+            Console.WriteLine(name);
+            Console.WriteLine(sex);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
