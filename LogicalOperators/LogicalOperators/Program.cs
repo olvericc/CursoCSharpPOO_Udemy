77f75368748b1ab937a47bb667fe302d339c749c
Eric Lopes <olv.ericlopes@gmail.com>
2021-11-29 11:25:37 -0300

    Logical operators in c#

---
+++ LogicalOperators/LogicalOperators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LogicalOperators {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // logical operators
+
+            bool c1 = 4 != 5;   // true
+            Console.WriteLine(c1); 
+
+            // operator '&&'
+
+            bool c2 = 2 > 3 && 4 != 5; // false
+            Console.WriteLine(c2);
+
+            // operator '||'
+
+            bool c3 = 2 > 3 || 4!= 5; // true
+            Console.WriteLine(c3);
+
+            // operator '!'
+
+            bool c4 = !(2 > 3) && 4 != 5; // true
+            Console.WriteLine(c4);
+
+            // example
+            Console.WriteLine("---------");
+            bool c5 = 10 < 5; // false
+            bool c6 = c3 || c4 && c5; // true
+            Console.WriteLine(c5);
+            Console.WriteLine(c6);
+        }
+    }
+}
