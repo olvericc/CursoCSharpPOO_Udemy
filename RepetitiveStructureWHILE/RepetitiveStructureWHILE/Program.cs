b4f5f8c9a660f7dcdf0bce321ab87b30b72ec8e1
Eric Lopes <olv.ericlopes@gmail.com>
2021-11-30 13:03:30 -0300

    Repetitive structure in c#

---
+++ RepetitiveStructureWHILE/RepetitiveStructureWHILE/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace RepetitiveStructureWHILE {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            Console.Write("enter with one number: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x >= 0.0) { 
+                double root = Math.Sqrt(x);
+                Console.WriteLine("square root: " + root.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("enter another number: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("negative number!");
+        }
+    }
+}
