88c14d7b7a036c830dc33919fbeaaf4f5ce9948d
Eric Lopes <olv.ericlopes@gmail.com>
2021-11-30 19:31:48 -0300

    Repetitive structure FOR in c#

---
+++ RepetitiveStructureFOR/RepetitiveStructureFOR/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RepetitiveStructureFOR {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // repetitive structure 'for'
+
+            Console.Write("How many whole numbers are you going to type? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i <= n; i++) {
+                Console.Write("value #" + i + " : ");
+                int value = int.Parse(Console.ReadLine());
+                sum += value;
+            }
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
