d90e65ff1eb76593a2dd0cfa9ce9dde86858c528
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-06 12:06:49 -0300

    Object and method ToString() in c# in example problem

---
+++ ExampleProblem2/ExampleProblem2/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExampleProblem2 {
+    internal class Product {
+
+        // class attributes
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        // method TotalValueInStock()
+        public double TotalValueInStock() {
+            return Price * Quantity;
+        }
+
+        // method ToString()
+        public override string ToString() {
+            return Name 
+                + ", $ " 
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantity
+                + " units in stock, total: $"
+                + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ ExampleProblem2/ExampleProblem2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExampleProblem2 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // instantiation
+            Product p = new Product();
+
+            Console.WriteLine("enter with product data");
+            Console.Write("name: ");
+            p.Name = Console.ReadLine();
+            Console.Write("price: ");
+            p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("quantity in stock: ");
+            p.Quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("product data: " + p);
+
+        }
+    }
+}
