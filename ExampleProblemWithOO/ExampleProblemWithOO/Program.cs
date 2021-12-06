b0308ef982de22e568d2395ac254742f7284ea09
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-06 11:35:43 -0300

    Example problem using basic concepts of OO in c# day: 6/12/2021

---
+++ ExampleProblemWithOO/ExampleProblemWithOO/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExampleProblemWithOO {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Triangle x, y;
+            
+            // instantiation
+
+            x = new Triangle();
+            y = new Triangle();
+            
+            Console.WriteLine("enter with the measure of triangle x: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("enter with the measure of triangle x: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine("area triangle x: " + areaX.ToString("F4", 
+                CultureInfo.InvariantCulture));
+            Console.WriteLine("area triangle y: " + areaY.ToString("F4",
+                CultureInfo.InvariantCulture));
+
+            if (areaX > areaY) {
+                Console.WriteLine("greater area: X");
+            }
+            else if (areaX == areaY){
+                Console.WriteLine("area with equal value");
+            } 
+            else {
+                Console.WriteLine("greater area: Y");
+            }
+        }
+    }
+}
---
+++ ExampleProblemWithOO/ExampleProblemWithOO/Triangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExampleProblemWithOO {
+    internal class Triangle {
+
+        // class attributes
+        public double A;
+        public double B;
+        public double C;
+
+        // method Area()
+        
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
