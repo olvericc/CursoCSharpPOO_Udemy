4bc0e124941b797ec6c4deed0f1b83e1af709d4a
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-07 09:04:48 -0300

    Fixation exercise 01 List in c#

---
+++ FixExercise01ListOOP/FixExercise01ListOOP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixExercise01ListOOP {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // fixation exercise 01
+
+            // instantiation
+            Retangle r = new Retangle();
+
+            Console.WriteLine("enter with width and height of retangle");
+            r.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("area: " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("perimeter: " + r.Perimeter().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("diagonal: " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ FixExercise01ListOOP/FixExercise01ListOOP/Retangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FixExercise01ListOOP {
+    internal class Retangle {
+
+        // class attributes
+        public double Width;
+        public double Height;
+
+        // method Area()
+        public double Area() {
+            return Height * Width;
+        }
+
+        // method Perimeter()
+        public double Perimeter() {
+            return 2 * (Height + Width);
+        }
+
+        // method Diagonal()
+        public double Diagonal() {
+            return Math.Sqrt(Width * Width + Height * Height);
+        }
+    }
+}
