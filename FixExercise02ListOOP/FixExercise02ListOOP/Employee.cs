9e45c32e643144520fa901b2d0d6ea43892b942e
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-07 09:27:01 -0300

    Fixation exercise 02 List in c# day: 7/12/2021

---
+++ FixExercise02ListOOP/FixExercise02ListOOP/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixExercise02ListOOP {
+    internal class Employee {
+
+        // class attributes
+        public string Name;
+        public double GrossSalary;
+        public double Tax;
+
+        // method LiquidSalary()
+        public double LiquidSalary() {
+            return GrossSalary - Tax;
+        }
+
+        // method IncreaseSalary()
+        public void IncreaseSalary(double percentage) {
+            GrossSalary = GrossSalary + (GrossSalary * percentage / 100.0);
+        }
+
+        // method ToString()
+        public override string ToString() {
+            return Name
+                + ", $ "
+                + LiquidSalary().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ FixExercise02ListOOP/FixExercise02ListOOP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixExercise02ListOOP {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            // fixation exercise 02
+
+            // instantiation
+            Employee emp1 = new Employee();
+
+            Console.Write("name: ");
+            emp1.Name = Console.ReadLine();
+            Console.Write("gross salary: ");
+            emp1.GrossSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("tax: ");
+            emp1.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("employee: " + emp1);
+
+            Console.WriteLine();
+            Console.Write("enter the percentage to increase the salary: ");
+            double p = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            emp1.IncreaseSalary(p);
+
+            Console.WriteLine();
+            Console.WriteLine("updated data: " + emp1);
+        }
+    }
+}
