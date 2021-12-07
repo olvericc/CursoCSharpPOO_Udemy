cc0a9ae1f58c1f0094840d19c2427ff6670f1fcd
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-07 15:53:46 -0300

    Fixation exercise 03 List in c#

---
+++ FixExercise03ListOOP/FixExercise03ListOOP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixExercise03ListOOP {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // fixation exercise 03
+            
+            // instatiation
+            Student s = new Student();
+
+            Console.Write("student name: ");
+            s.Name = Console.ReadLine();
+            Console.WriteLine("enter with the student's three grades: ");
+            s.Grade1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            s.Grade2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            s.Grade3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("final grade: " + s.FinalGrade().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (s.Situation()) {
+                Console.WriteLine("approved!");
+            } else {
+                Console.WriteLine("disapproved!");
+                Console.WriteLine("missed " 
+                    + s.RemainingGrade().ToString("F2", CultureInfo.InvariantCulture) 
+                    + " points...");
+            }
+        }
+    }
+}
---
+++ FixExercise03ListOOP/FixExercise03ListOOP/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixExercise03ListOOP {
+    internal class Student {
+
+        // class attributes
+        public string Name;
+        public double Grade1;
+        public double Grade2;
+        public double Grade3;
+
+        // method FinalGrade()
+        public double FinalGrade() {
+            return Grade1 + Grade2 + Grade3;
+        }
+
+        // method Situation()
+        public bool Situation() {
+            if (FinalGrade() >= 60.0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // method RemainingGrade()
+        public double RemainingGrade() {
+            if (Situation()) {
+                return 0.0;
+            } else {
+                return 60.0 - FinalGrade();
+            }
+        }
+    }   
+}
