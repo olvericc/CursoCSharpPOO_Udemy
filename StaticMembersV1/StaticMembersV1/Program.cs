1adb742802dbfc323ab529dceee6d2043b351af0
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-09 09:24:00 -0300

    Static Members Version 1 - methods in the program class itself

---
+++ StaticMembersV1/StaticMembersV1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace StaticMembersV1 {
+    internal class Program {
+
+        // const Pi
+        static double Pi = 3.14;
+
+        static void Main(string[] args) {
+
+            Console.Write("enter with radius value: ");
+            double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            // instatiation
+            double circ = Circuference(radius);
+            double vol = Volume(radius);
+
+            // printing 
+            Console.WriteLine("circuference: " + circ.ToString("F2",
+                CultureInfo.InvariantCulture));
+
+            Console.WriteLine("volume: " + vol.ToString("F2",
+                CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Pi value: " + Pi.ToString("F2", 
+                CultureInfo.InvariantCulture));
+        }
+
+        // function to calculate Circuference
+
+        static double Circuference(double r) {
+            return 2.0 * Pi * r;    
+        }
+
+        // function to calculate Volume
+
+        static double Volume(double r) {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
