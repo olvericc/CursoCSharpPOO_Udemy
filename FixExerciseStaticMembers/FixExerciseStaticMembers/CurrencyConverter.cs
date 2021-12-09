14ef09db7053ec6738cd8ed22db4bbd8982e7f75
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-09 10:16:46 -0300

    Fixation exercise of static members in c#

---
+++ FixExerciseStaticMembers/FixExerciseStaticMembers/CurrencyConverter.cs
@@ -0,0 +1,11 @@
+﻿namespace FixExerciseStaticMembers {
+    internal class CurrencyConverter {
+
+        public static double IOF = 6.0;
+
+        public static double DollarForReal(double amount, double rate) {
+            double calc = amount * rate ;
+            return calc + calc * IOF / 100.0;
+        }
+    }
+}
---
+++ FixExerciseStaticMembers/FixExerciseStaticMembers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixExerciseStaticMembers {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("what is the dollar rate? ");
+            double rate = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("how many dollars you gonna buy? ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = CurrencyConverter.DollarForReal(amount, rate);
+
+            Console.WriteLine("value to be pay in real: " + result.ToString("F2",
+                CultureInfo.InvariantCulture));
+        }
+    }
+}
