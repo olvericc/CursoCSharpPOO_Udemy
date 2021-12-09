78c897014d97078604d8f2f2cbd46ef65a801d56
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-09 09:57:29 -0300

    Static Members Version 3 - Calculator class with static method day: 9/12/2021

---
+++ StaticMembersV3/StaticMembersV3/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticMembersV3 {
+    internal class Calculator {
+
+        public static double Pi = 3.14;
+
+        public static double Circuference(double r) {
+            return 2.0 * Pi * r;
+        }
+
+        public static double Volume(double r) {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
---
+++ StaticMembersV3/StaticMembersV3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace StaticMembersV3 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("enter with radius value: ");
+            double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculator.Circuference(radius);
+            double vol = Calculator.Volume(radius);
+
+            Console.WriteLine("circuference: " + circ.ToString("F2",
+                CultureInfo.InvariantCulture));
+            Console.WriteLine("volume: " + vol.ToString("F2",
+                CultureInfo.InvariantCulture));
+            Console.WriteLine("pi value: " + Calculator.Pi.ToString("F2",
+                CultureInfo.InvariantCulture));
+        }
+    }
+}
