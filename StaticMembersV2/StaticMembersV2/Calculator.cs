9ea2abd16eee5d44dc4acf839a0593fea3e56c14
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-09 09:39:54 -0300

    Static Members Version 2 - Calculator class with instance members

---
+++ StaticMembersV2/StaticMembersV2/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticMembersV2 {
+    internal class Calculator {
+
+        public double Pi = 3.14;
+
+        public double Circuference(double r) {
+            return 2.0 * Pi * r;
+        }
+
+        public double Volume(double r) {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
---
+++ StaticMembersV2/StaticMembersV2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace StaticMembersV2 {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            // instantiation 
+            Calculator calc = new Calculator();
+
+            Console.Write("enter with radius value: ");
+            double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = calc.Circuference(radius);
+            double vol = calc.Volume(radius);
+
+            Console.WriteLine("circuference: " + circ.ToString("F2",
+                CultureInfo.InvariantCulture));
+            Console.WriteLine("volume: " + vol.ToString("F2",
+                CultureInfo.InvariantCulture));
+            Console.WriteLine("pi value: " + calc.Pi.ToString("F2",
+                CultureInfo.InvariantCulture));
+        }
+    }
+}
