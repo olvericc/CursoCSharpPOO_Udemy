4b15863847a828616b9098aaafac922ae820c512
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-13 18:57:15 -0300

    Constructors in c#

---
+++ Constructors/Constructors/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Constructors {
+    internal class Product {
+
+        // class attributes
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        // constructor with arguments
+        public Product(string name, double price, int quantity) {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        // method TotalValueInStock()
+        public double TotalValueInStock() {
+            return Price * Quantity;
+        }
+
+        // method AddProducts()
+        public void AddProducts(int quantity) {
+            Quantity += quantity;
+        }
+
+        // method RemoveProducts()
+        public void RemoveProductcs(int quantity) {
+            Quantity -= quantity;
+        }
+
+        // method ToString()
+        public override string ToString() {
+            return Name
+                    + ", $ "
+                    + Price.ToString("F2", CultureInfo.InvariantCulture)
+                    + ", "
+                    + Quantity
+                    + " units, total: $ "
+                    + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Constructors {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("enter with product data:");
+            Console.Write("name: ");
+            string name = Console.ReadLine();
+            Console.Write("price: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("quantity in stock: ");
+            int quantity = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Product p = new Product(name, price, quantity);
+
+
+            Console.WriteLine();
+            Console.WriteLine("product data: " + p);
+
+            Console.WriteLine();
+            Console.Write("enter the number of products to be added in stock: ");
+            int qtt = int.Parse(Console.ReadLine());
+            p.AddProducts(qtt);
+            Console.WriteLine();
+            Console.WriteLine("updated data: " + p);
+
+            Console.WriteLine();
+            Console.Write("enter the number of products to be removed in stock: ");
+            qtt = int.Parse(Console.ReadLine());
+            p.RemoveProductcs(qtt);
+            Console.WriteLine();
+            Console.WriteLine("updated data: " + p);
+            
+
+
+        }
+    }
+}
