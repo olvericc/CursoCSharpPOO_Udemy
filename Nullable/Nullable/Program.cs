59a2909c658b0d8ff195362e39989e1b3788f273
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-16 20:46:39 -0300

    Nullable and null-coalescing operator

---
+++ Nullable/Nullable/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Nullable {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // double x = null; double is not nullable
+            double? x = null; // or Nullable<double> x =  null;
+            double? y = 10.0;
+            //bool? z = null;
+
+            // method GetValueOrDefault()
+
+            Console.WriteLine(x.GetValueOrDefault());
+            //Console.WriteLine(z.GetValueOrDefault());
+
+            Console.WriteLine(y.GetValueOrDefault());
+
+            // method HasValue
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            // method Value
+
+            if (x.HasValue) {
+                Console.WriteLine(x.Value);
+            } else {
+                Console.WriteLine("x is null!");
+            }
+
+            if (y.HasValue) {
+                Console.WriteLine(y.Value);
+            } else {
+                Console.WriteLine("y is null!");
+            }
+
+            // null-coalescing operator
+            double? value1 = null;
+            double? value2 = 10;
+            double a = value1 ?? 5;
+            double b = value2 ?? 5;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
