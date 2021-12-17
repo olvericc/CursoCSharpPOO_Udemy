8a777c0a09d5fee17d46eae1fed06969e14d2cf5
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-17 09:56:04 -0300

    Bad solution without Params Modifiers in c# day: 17/12/2021

---
+++ ParamModifier/ParamModifier/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ParamModifier {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 3, 4);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
