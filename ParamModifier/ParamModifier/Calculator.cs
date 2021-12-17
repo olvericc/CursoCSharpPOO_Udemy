7fef0b1c3653379f66e44870df3192ac7df5de89
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-17 10:05:09 -0300

    Good solution with Params Modifiers in c#

--- ParamModifier/ParamModifier/Calculator.cs
+++ ParamModifier/ParamModifier/Calculator.cs
@@ -1,15 +1,13 @@
 ﻿namespace ParamModifier {
     class Calculator {
 
-        // overload
-        public static int Sum(int n1, int n2) {
-            return n1 + n2;
+        // method Sum()
+        public static int Sum(params int[] numbers) {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+            return sum;
         }
-        public static int Sum(int n1, int n2, int n3) {
-            return n1 + n2 + n3;
-        }
-        public static int Sum(int n1, int n2, int n3, int n4) {
-            return n1 + n2 + n3 + n4;
-        }  
     }
 }
--- ParamModifier/ParamModifier/Program.cs
+++ ParamModifier/ParamModifier/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 
 namespace ParamModifier {
-    internal class Program {
+    class Program {
         static void Main(string[] args) {
 
-            int s1 = Calculator.Sum(2, 3);
-            int s2 = Calculator.Sum(2, 3, 4);
+            int s1 = Calculator.Sum( 2, 3 );
+            int s2 = Calculator.Sum( 1, 2, 3 );
+
             Console.WriteLine(s1);
             Console.WriteLine(s2);
         }
