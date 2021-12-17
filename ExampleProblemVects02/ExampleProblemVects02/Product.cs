baa5989824afbcb5b2d2c6062c79ae8be72b7b55
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-16 21:33:49 -0300

    Example problem 02 with one class and vect in c#

---
+++ ExampleProblemVects02/ExampleProblemVects02/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleProblemVects02 {
+    class Product {
+
+        // class attributes/properties
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ ExampleProblemVects02/ExampleProblemVects02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExampleProblemVects02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            // creating vect
+            Product [] vect = new Product[n];
+
+            for (int i = 0; i < n; i++) {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                vect[i] = new Product { Name = name, Price = price };
+            }
+
+            double sum = 0;
+            for (int i = 0;i < n;i++) {
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+            Console.WriteLine("avarage price: $" + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
