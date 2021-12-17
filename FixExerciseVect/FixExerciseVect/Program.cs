e2bdada02e6486d00ab7fd22d6f4055cf84383ff
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-17 09:37:00 -0300

    Fixation exercise of vects in c#

---
+++ FixExerciseVect/FixExerciseVect/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FixExerciseVect {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // creating a vect
+            Rent[] vect = new Rent[10];
+
+            Console.Write("how many rooms will be rented: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++) {
+                Console.WriteLine();
+                Console.WriteLine("rent #" + i + ":");
+                Console.Write("student name: ");
+                string Sname = Console.ReadLine();
+                Console.Write("student e-mail: ");
+                string Semail = Console.ReadLine();
+                Console.Write("room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                vect[room] = new Rent(Sname, Semail);
+            }
+            Console.WriteLine();
+            Console.WriteLine("busy rooms:");
+
+            for (int i = 0; i < 10; i++) {
+                if (vect[i] != null) {
+                    Console.WriteLine(i + " :" + vect[i]);
+                }
+            }
+        }
+    }
+}
---
+++ FixExerciseVect/FixExerciseVect/Rent.cs
@@ -0,0 +1,23 @@
+﻿namespace FixExerciseVect {
+    class Rent {
+
+        // class attributes
+        public string StudentName { get; set; }
+        public string StudentEmail { get; set; }
+
+        // default builder
+        public Rent() {
+        }
+
+        // constructor with two arguments
+        public Rent(string studentName, string studentEmail) {
+            StudentName = studentName;
+            StudentEmail = studentEmail;
+        }
+
+        // method ToString()
+        public override string ToString() {
+            return StudentName + ", " + StudentEmail;
+        }
+    }
+}
