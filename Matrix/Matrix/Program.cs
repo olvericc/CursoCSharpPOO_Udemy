dd847327b6f0d774db4e4423d4caa36c1c0013a9
Eric Lopes <olv.ericlopes@gmail.com>
2021-12-21 17:21:42 -0300

    Matrix in c# day: 21/12/2021

---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Matrix {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // creating a matrix
+            double[,] mat = new double[2, 3];
+
+            // length
+            Console.WriteLine(mat.Length);
+
+            // rank
+            Console.WriteLine(mat.Rank);
+
+            // getLenght1
+            Console.WriteLine(mat.GetLength(0));
+
+            // getLength2
+            Console.WriteLine(mat.GetLength(1));
+
+        }
+    }
+}
